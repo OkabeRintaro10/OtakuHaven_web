56762f9045f76f383d0b0253e9cce9cd681fbf0f
OkabeRintaro10 <kausikremella@gmail.com>
2023-03-02 12:06:27 +0530

    responsive navbar

--- index_script.js
+++ index_script.js
@@ -1,14 +1,7 @@
-$(document).ready(function () {
-  //hamburger Toggle
-  $(".humbarger").click(function (event) {
-    $(".menu-list").slideToggle(500);
-    event.preventDefault();
+var menu = document.querySelector("#menu-icon");
+var navbar = document.querySelector(".navbar");
 
-    $(".menu-list li a").click(function (event) {
-      if ($(window).width() < 768) {
-        $(".menu-list").slideUp(500);
-        event.preventDefault();
-      }
-    });
-  });
+menu.addEventListener("click", function () {
+  menu.classList.toggle("ri-close-fill");
+  navbar.classList.toggle("open");
 });
